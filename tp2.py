c672f21605ac5d048c18372b30e846307c5106b8
Gabriel Goncalves <gr.goncalves@campus.fct.unl.pt>
2021-06-08 20:43:37 +0100

    it survives

--- tp2.py
+++ tp2.py
@@ -18,39 +18,36 @@ actions = {-1:'left',
             0:'same direction',
             1:'right' }
 
-train_episodes = 30000
+train_episodes = 300
 
 
 def agent(state_shape, action_shape):
-    learning_rate = 0.001
+    learning_rate = 0.01
 
     init = tf.keras.initializers.he_uniform()
     model = keras.Sequential()
     model.add(keras.layers.Input(shape=state_shape))
 
-    model.add(keras.layers.Conv2D(16,(2,2), padding="same"))
+    model.add(keras.layers.Conv2D(64,(2,2), padding="same"))
     model.add(keras.layers.Activation("relu"))
+    model.add(keras.layers.Dropout(0.5))
     model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
-    model.add(keras.layers.Conv2D(32,(3,3), padding="same", strides=(2,2)))
+    model.add(keras.layers.Conv2D(64,(3,3), padding="same"))
     model.add(keras.layers.Activation("relu"))
-    model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
-    model.add(keras.layers.Conv2D(64,(4,4), padding="same", strides=(2,2)))
-    model.add(keras.layers.Activation("relu"))
-    model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
-    model.add(keras.layers.Conv2D(128,(6,6), padding="same", strides=(3,3)))
-    model.add(keras.layers.Activation("relu"))
+    model.add(keras.layers.MaxPooling2D(pool_size=(5,5)))
     
 
     model.add(keras.layers.Flatten(name='features'))
     model.add(keras.layers.Dense(128, activation='relu', kernel_initializer=init))
-    model.add(keras.layers.Dense(32, activation='relu', kernel_initializer=init))
+    model.add(keras.layers.Dense(64, activation='relu', kernel_initializer=init))
+    model.add(keras.layers.Dense(16, activation='relu', kernel_initializer=init))
     model.add(keras.layers.Dense(action_shape, activation='linear', kernel_initializer=init))
     model.compile(loss=tf.keras.losses.Huber(), optimizer=tf.keras.optimizers.Adam(lr=learning_rate), metrics=['accuracy'])
     print(model.summary())
     return model
 
 def train(env, replay_memory, model, target_model, done):
-    discount_factor = 0.9
+    discount_factor = 1
     batch_size = 256
     mini_batch = random.sample(replay_memory, batch_size)
     current_states = np.array([transition[0] for transition in mini_batch])
@@ -70,76 +67,106 @@ def train(env, replay_memory, model, target_model, done):
         Y.append(current_qs)
     model.fit(np.array(X), np.array(Y), batch_size=batch_size, verbose=0, shuffle=True)
 
+def check_move(x, y, action, direction, board_shape, head, tail):
+    max_x = board_shape[0]-3
+    max_y = board_shape[1]-3
+    new_x = x
+    new_y = y
+    new_direction = direction + action
+    if new_direction == 4:
+        new_direction = 0
+    elif new_direction == -1:
+        new_direction = 3
+    
+    if new_direction == 0:
+        new_y-=1
+    elif new_direction == 1:
+        new_x+=1
+    elif new_direction == 2:
+        new_y+=1
+    else:
+        new_x-=1
+
+    return (new_x >= 0 and new_x <= max_x and new_y >= 0 and new_y <= max_y and ((new_y,new_x) not in tail))
+        
+
+
 def heuristic(env, replay_memory, n_examples, board_shape):
     n=0
-    random_threshold = 0.3
+    apples = 0
+    deaths = 0
     while(n < n_examples):
         number_steps = 0
         done = False
-        game_reward = 0
         observation = env.reset()[0]
         while not done:
             number_steps +=1
             random_number = np.random.rand()
             score,apple,head,tail,direction = env.get_state()
-            y=head[0]
-            x=head[1]
-            d_y = apple[0][0] - y
-            d_x = apple[0][1] - x
-            if abs(d_y)>abs(d_x):#more vertical distance
-                if d_y<0:
-                    ddir = 0#up
+            y,x = head
+            y_apple, x_apple = apple[0]
+            dist_y = y_apple-y
+            dist_x = x_apple-x
+            #direction (0=up, 1=right, 2=down, 3=left)
+            #action -1 (turn left), 0 (continue), 1 (turn right)
+            action=0
+            #-y -> up
+            #+y -> down
+            #-x -> left
+            #+x -> right
+            optimal_dir = 0
+            if((abs(dist_y)>abs(dist_x)) and (dist_x != 0)) or dist_y == 0:
+                if(dist_x>0):
+                    optimal_dir = 1
                 else:
-                    ddir = 2#down
+                    optimal_dir = 3
             else:
-                if d_x<0:
-                    ddir = 3#left
+                if(dist_y>0):
+                    optimal_dir = 2
                 else:
-                    ddir = 1#right
-            action = ddir-direction
-            if action>1: action = 1#right
-            if action<-1: action = -1#left
-            n_it = 0
-            while True:
-                n_it += 1
-                y=head[0]
-                x=head[1]
-                new_dir = direction + action
-                if new_dir<0:
-                    new_dir = 3
-                elif new_dir > 3:
-                    new_dir = 0
+                    optimal_dir = 0
+            
+            diff_dir = optimal_dir - direction #how many times do I turn right to get there
+            if(diff_dir > 0):
+                if(diff_dir == 3):
+                    action = -1
+                else:
+                    action = 1
+            elif(diff_dir < 0):
+                if(diff_dir == -3):
+                    action = 1
+                else:
+                    action = -1
+            moves = list(filter(lambda a: a!= action,actions.keys()))
+            valid_move = check_move(x,y,action,direction,board_shape,head,tail)
+            if not valid_move:
+                for move in moves:
+                    action = move
+                    valid_move = check_move(x,y,action,direction,board_shape,head,tail)
+                    if valid_move:
+                        break
 
-                if new_dir == 0:
-                    y = y-1
-                elif new_dir == 1:
-                    x = x+1
-                elif new_dir == 2:
-                    y = y+1
-                else:
-                    x = x-1
-                if ((y,x) in tail or x == 0 or y == 0 or x == board_shape[0]-2 or y == board_shape[0]-2) and n_it <15:
-                    action = random.choices(list(actions.keys()))[0]
-                else:
-                    break
-
-            # if(random_number < random_threshold):
-            #     action = random.choices(list(actions.keys()))[0] 
             new_observation, reward, done, score = env.step(action) # new_observation = novo mapa
-            if(reward > 0):
-                replay_memory.append([observation, action, reward/number_steps, new_observation, done])
-                game_reward += reward
-                n+=1
+            replay_memory.append([observation, action+1, reward, new_observation, done])
+            if(reward == -1):
+                deaths +=1
+            elif(reward ==1):
+                apples+=1
+                
+            n+=1
+    print("Apples, %d" % apples)
+    print("Deaths, %d" % deaths)
+    print("Average Apples, %d" % (apples/deaths))
 
 
 def main():
-    epsilon = 1
-    max_epsilon = 1
+    epsilon = 0.5
+    max_epsilon = 0.5
     min_epsilon = 0.01
-    decay = 0.01
+    decay = 0.001
     MIN_REPLAY_SIZE = 1024
     number_of_actions = 3
-    env = SnakeGame(30,30,border=1)
+    env = SnakeGame(30,30,border=1, max_grass = 0, food_amount = 1)
     board_shape = (env.board.shape[0]+2*env.border,env.board.shape[1]+2*env.border,env.board.shape[2])
 
     model = agent(board_shape, number_of_actions)
@@ -147,7 +174,7 @@ def main():
     target_model.set_weights(model.get_weights())
     
     replay_memory = deque(maxlen=50000)
-    heuristic(env, replay_memory, 15000, board_shape)
+    heuristic(env, replay_memory, 50000, board_shape)
     steps_to_update_target_model = 0
     total_training_rewards = 0
    
@@ -164,17 +191,13 @@ def main():
                 action = random.choices(list(actions.keys()))[0]
             else:
                 predicted = model.predict(observation.reshape([1,*board_shape])).flatten()
-                #action = np.argmax(predicted)-1
-                probabilities = np.exp(predicted)/sum(np.exp(predicted))#softmax
-                action = choices(list(actions.keys()), probabilities)
-                print(predicted)
-                action = action[0]
+                action = np.argmax(predicted)-1
+                # probabilities = np.exp(predicted)/sum(np.exp(predicted))#softmax
+                # action = choices(list(actions.keys()), probabilities)
+                # action = action[0]
             #print('chose action ' + actions[action])
-            
             new_observation, reward, done, score = env.step(action) # new_observation = novo mapa
-            if reward == 1:
-                print("APPLE")
-            replay_memory.append([observation, action, reward, new_observation, done])
+            replay_memory.append([observation, action+1, reward, new_observation, done])
             game_reward += reward
             #env.print_state()
             if len(replay_memory) >= MIN_REPLAY_SIZE and (steps_to_update_target_model % 4 == 0 or done):
